d45d2d6595cd5f0dd8d78ccfe706e2ef33bbf44a
DouglasMaceddo <124937492+DouglasMaceddo@users.noreply.github.com>
2024-10-01 20:26:15 -0300

    Localstorage

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -23,6 +22,21 @@ export class CadastroPage implements OnInit {
       login: ['', Validators.required],
       senha: ['', Validators.required]
     });
+  }
+  cadastrar() {
+    if (this.cadastroForm.valid) {
+      // Obtem os valores do formulário
+      const cadastroData = this.cadastroForm.value;
+
+      // Armazena os dados no localStorage
+      localStorage.setItem('cadastro', JSON.stringify(cadastroData));
+
+      // Você pode navegar para outra página ou mostrar uma mensagem de sucesso
+      console.log('Cadastro salvo com sucesso!', cadastroData);
+      this.router.navigate(['/login']);
+    } else {
+      console.log('Formulário inválido');
+    }
   }
 
   navigateToNovaPagina() {
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CadastroPage } from '../cadastro/cadastro.page';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +9,41 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  email: string = '';
+  senha: string = '';
+
+  constructor(private router: Router, private alertController: AlertController) {}
+
+  ngOnInit() {}
+
+  // Método para realizar o login
+  login() {
+    // Buscar os dados de cadastro do localStorage
+    const dadosCadastro = JSON.parse(localStorage.getItem('cadastro') || '{}');
+
+    // Verificar se o localStorage tem os dados e se correspondem ao login informado
+    if (dadosCadastro && dadosCadastro.login === this.email && dadosCadastro.senha === this.senha) {
+      console.log('Login bem-sucedido!');
+      this.router.navigate(['/catalogo']); // Navegar para a página inicial ou home após o login bem-sucedido
+    } else {
+      this.presentAlert('Erro', 'Email ou senha inválidos!');
+    }
+  }
+
+  // Método para navegação à página de cadastro
   navigateToNovaPagina() {
     this.router.navigate(['/cadastro']);
   }
-  ngOnInit() {
-  }
 
+  // Apresenta alerta no caso de erro de login
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
+
