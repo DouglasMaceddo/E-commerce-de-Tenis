5625375504c07b54dcb9fa188a9663774b535323
Gabriel Henrique <gabriel_gyn15@hotmail.com>
2024-11-03 15:25:24 -0300

    adicionando api basica

---
+++ api/src/main/java/com/laplazza/api/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.laplazza.api.dto;
+
+public record RegisterRequestDTO (String name,String email, String password, String cpf, String telefone) {
+}
---
+++ api/src/main/java/com/laplazza/api/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package com.laplazza.api.dto;
+
+public record ResponseDTO (String name, String token) { }
